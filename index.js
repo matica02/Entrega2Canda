dead1cfb6a7ae5b2d35bc892877e039310dd1e0f
Matias Canda <canda.matias2002@gmail.com>
2023-10-27 20:19:48 -0300

    corregir funcion agregarACarrito

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 
-form.addEventListener("submit", (e) => {crearUsuario(e)})
+form.addEventListener("submit", (e) => {crearUsuario(e)});
 
 function crearUsuario(e){
     e.preventDefault()
@@ -20,48 +20,49 @@ function crearUsuario(e){
     console.log(usuario)
     localStorage.setItem("user", JSON.stringify(usuario))
     form.reset()
-}
+};
 
 const autos= [
 {
     id: 1,
     nombre: "Lamborghini Aventador",
     precio: 120,
-    img: "./img/lamborghini.png"
+    img: "./img/lamborghini.png",
 },
 {
     id: 2,
     nombre: "Mercedes AMG GTR",
     precio: 90,
-    img: "../img/mercedes.png"
+    img: "../img/mercedes.png",
 },
 {
     id: 3,
     nombre: "Volkswagen Golf R",
     precio: 70,
-    img: "../img/volkswagen.png"
+    img: "../img/volkswagen.png",
 },
 {
     id: 4,
     nombre: "Porsche GT3RS",
     precio: 110,
-    img: "../img/porsche.png"
+    img: "../img/porsche.png",  
 },
 {
     id: 5,
     nombre: "Audi R8",
     precio: 100,
-    img: "../img/audir8.png"
+    img: "../img/audir8.png",
 },
 {
     id: 6,
     nombre: "BMW M4",
     precio: 80,
-    img: "../img/m4.png"
+    img: "../img/m4.png",
 }
 ];
 
 const contenedorAutos= document.querySelector("#contenedorAutos");
+const contenedorCarrito= document.querySelector("#contenedorCarrito");
 
 function crearCards(){
     autos.forEach((auto) => {
@@ -82,18 +83,28 @@ function crearCards(){
 
 crearCards();
 
-const contenedorCarrito= document.querySelector("#contenedorCarrito");
+const btnLambo= document.getElementById("btn-1");
+const btnMercedes= document.getElementById("btn-2");
+const btnVolkswagen = document.getElementById("btn-3");
+const btnPorsche = document.getElementById("btn-4");
+const btnAudi = document.getElementById("btn-5");
+const btnBMW = document.getElementById("btn-6");
 
-const btnLambo = document.getElementById("btn-1")
+btnLambo.addEventListener("click" , agregarACarrito(0));
+btnMercedes.addEventListener("click" , agregarACarrito(1));
+btnVolkswagen.addEventListener("click" , agregarACarrito(2));
+btnPorsche.addEventListener("click" , agregarACarrito(3));
+btnAudi.addEventListener("click" , agregarACarrito(4));
+btnBMW.addEventListener("click" , agregarACarrito(5));
 
-btnLambo.addEventListener("click", agregarACarrito)
+function agregarACarrito(producto){
+    let contenedorProducto = document.createElement("li")
+    contenedorProducto.innerHTML = `<li class="nav-item">
+    <p>${autos[producto].nombre}, Precio: ${autos[producto].precio} USD</p></li>`;
 
-function agregarACarrito(){
-    let contenedorLambo = document.createElement("li")
-    contenedorLambo.innerHTML = `<li class="nav-item">
-    <p class="nav-link">Lamborghini Aventador, Precio: 120 USD.</p>
-    </li>`;
-contenedorCarrito.appendChild(contenedorLambo)
+contenedorCarrito.appendChild(contenedorProducto)
 };
+
+
 
 
