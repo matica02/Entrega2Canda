e5f33e0adf0e1352e0b39661504e5db3b8b0704e
Matias Canda <canda.matias2002@gmail.com>
2023-10-27 11:26:56 -0300

    commit 2

--- index.js
+++ index.js
@@ -5,40 +5,6 @@ let botonMercedes= document.getElementById("btn-mercedes");
 let botonVolkswagen= document.getElementById("btn-volkswagen");
 let botonPorsche= document.getElementById("btn-porsche"); */
 
-/* const prodcutos= [
-{
-    id: 1,
-    nombre: "Lamborghini Aventador",
-    Precio: 110,
-},
-{
-    id: 2,
-    nombre: "Mercedes AMG GTR",
-    Precio: 90,
-},
-{
-    id: 3,
-    nombre: "Volkswagen Golf R",
-    Precio: 80,
-},
-{
-    id: 4,
-    nombre: "Porsche GT3RS",
-    Precio: 100,
-},
-]; */
-
-/* 
-            <div class="card cards-vw mx-auto col-sm-12 col-md-2 col-lg-2">
-              <img class="rounded" src="../img/lamborghini.png" alt="Lamborghini Aventador">
-                <div class="card-body">
-                  <h3 class="card-text">${auto.nombre}</h3> 
-                  <p class="card-text">Price: ${auto.precio}usd</p> 
-                  <a class="card-text" id="btn-${auto.id}" href="#">Add to the cart.</a>
-                </div>
-            </div> 
-*/
-
 /* function agregarLambo() {
     let contenedorLambo = document.createElement("li")
     contenedorLambo.innerHTML = `<li class="nav-item">
@@ -49,8 +15,49 @@ contenedor.appendChild(contenedorLambo);
 
 botonLambo.addEventListener("click", agregarLambo); */
 
-const contenedorAutos= document.getElementById("contenedorAutos")
+const autos= [
+{
+    id: 1,
+    nombre: "Lamborghini Aventador",
+    precio: 110,
+},
+{
+    id: 2,
+    nombre: "Mercedes AMG GTR",
+    precio: 90,
+},
+{
+    id: 3,
+    nombre: "Volkswagen Golf R",
+    precio: 80,
+},
+{
+    id: 4,
+    nombre: "Porsche GT3RS",
+    precio: 100,
+},
+];
 
+const contenedorAutos= document.querySelector("#contenedorAutos")
+
+function crearCards(){
+    autos.forEach((auto) => {
+        let contenedorAuto = document.createElement("div")
+        contenedorAuto.innerHTML = `<div class="card cards-vw mx-auto col-sm-12 col-md-2 col-lg-2">
+        <img class="rounded" src="../img/lamborghini.png" alt="Lamborghini Aventador">
+          <div class="card-body">
+            <h3 class="card-text">${auto.nombre}</h3> 
+            <p class="card-text">Price: ${auto.precio}usd</p> 
+            <a class="card-text" id="btn-${auto.id}" href="#">Add to the cart.</a>
+          </div>
+      </div> `
+    
+
+        contenedorAutos.appendChild(contenedorAuto)
+    })
+}
+
+crearCards()
 
 
 
