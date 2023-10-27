2f1283207d55dad4a7370bcb3433fe4329d0175a
Matias Canda <canda.matias2002@gmail.com>
2023-10-27 11:12:09 -0300

    commit

---
+++ index.js
@@ -0,0 +1,56 @@
+/* const contenedor= document.getElementById("contenedorGeneral")
+
+let botonLambo= document.getElementsByClassName("btn-lamborghini");
+let botonMercedes= document.getElementById("btn-mercedes");
+let botonVolkswagen= document.getElementById("btn-volkswagen");
+let botonPorsche= document.getElementById("btn-porsche"); */
+
+/* const prodcutos= [
+{
+    id: 1,
+    nombre: "Lamborghini Aventador",
+    Precio: 110,
+},
+{
+    id: 2,
+    nombre: "Mercedes AMG GTR",
+    Precio: 90,
+},
+{
+    id: 3,
+    nombre: "Volkswagen Golf R",
+    Precio: 80,
+},
+{
+    id: 4,
+    nombre: "Porsche GT3RS",
+    Precio: 100,
+},
+]; */
+
+/* 
+            <div class="card cards-vw mx-auto col-sm-12 col-md-2 col-lg-2">
+              <img class="rounded" src="../img/lamborghini.png" alt="Lamborghini Aventador">
+                <div class="card-body">
+                  <h3 class="card-text">${auto.nombre}</h3> 
+                  <p class="card-text">Price: ${auto.precio}usd</p> 
+                  <a class="card-text" id="btn-${auto.id}" href="#">Add to the cart.</a>
+                </div>
+            </div> 
+*/
+
+/* function agregarLambo() {
+    let contenedorLambo = document.createElement("li")
+    contenedorLambo.innerHTML = `<li class="nav-item">
+    <p class="nav-link">Lamborghini Aventador, Price: 110usd</p>
+    </li>`;
+contenedor.appendChild(contenedorLambo);
+};
+
+botonLambo.addEventListener("click", agregarLambo); */
+
+const contenedorAutos= document.getElementById("contenedorAutos")
+
+
+
+
